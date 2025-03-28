0572c23b9c78ea2e971a2ec073e4006e02cbaa10
Rizwan <rzwnhawari@gmail.com>
2025-03-28 23:35:17 +0700

    Fix Theme Button terutama di Layar Mobile

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -72,8 +72,7 @@
                     </button>
                 </div>
             </div>
-        </div>
-               
+        </div>            
 
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 shadow-sm rounded-lg overflow-hidden">
